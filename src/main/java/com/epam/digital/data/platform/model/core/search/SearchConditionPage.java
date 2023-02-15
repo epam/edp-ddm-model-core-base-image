57370f53f9e5eee740c0fdb314ffd821dc1982b8
Yevhen Zvarych <yevhen_zvarych@epam.com>
2023-02-15 16:37:10 +0200

    [EDP-DDM-22710] - Added entity for SC paging response
    Change-Id: Ie0aa1f0579eb57c422d377fa234b678d889d61d1

---
+++ src/main/java/com/epam/digital/data/platform/model/core/search/SearchConditionPage.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright 2023 EPAM Systems.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.epam.digital.data.platform.model.core.search;
+
+import java.util.List;
+
+public class SearchConditionPage<T> {
+  private List<T> content;
+  private Integer totalElements;
+  private Integer totalPages;
+  private Integer pageNo;
+  private Integer pageSize;
+
+  public List<T> getContent() {
+    return content;
+  }
+
+  public void setContent(List<T> content) {
+    this.content = content;
+  }
+
+  public Integer getTotalElements() {
+    return totalElements;
+  }
+
+  public void setTotalElements(Integer totalElements) {
+    this.totalElements = totalElements;
+  }
+
+  public Integer getTotalPages() {
+    return totalPages;
+  }
+
+  public void setTotalPages(Integer totalPages) {
+    this.totalPages = totalPages;
+  }
+
+  public Integer getPageNo() {
+    return pageNo;
+  }
+
+  public void setPageNo(Integer pageNo) {
+    this.pageNo = pageNo;
+  }
+
+  public Integer getPageSize() {
+    return pageSize;
+  }
+
+  public void setPageSize(Integer pageSize) {
+    this.pageSize = pageSize;
+  }
+}
